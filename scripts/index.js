d59a91819b4c9ed741d94f1e5d50f74d326d6c1f
AnaMM <amlcm31@gmail.com>
2024-12-17 15:33:13 -0700

    added js

---
+++ scripts/index.js
@@ -0,0 +1,7 @@
+const initialCards = [{name: "Running photo by Martins Zemlickis", link: "https://unsplash.com/photos/people-running-on-road-during-daytime-NPFu4GfFZ7E"},
+                      {name: "Bread photo by Wesual Click", link: "https://unsplash.com/photos/cereal-and-three-buns-rsWZ-P9FbQ4"}, 
+                      {name: "Running photo by Jeremy Lapak", link: "https://unsplash.com/photos/person-running-on-top-on-hill-during-daytime-CVvFVQ_-oUg"}, 
+                      {name: "Bread photo by Rodolfo Marques", link: "https://unsplash.com/photos/sliced-breads-GzBO_o0RvEg"}, 
+                      {name: "Running photo by Nicolas Hoizey", link: "https://unsplash.com/photos/people-running-on-race-track-poa-Ycw1W8U"}, 
+                      {name: "Bread photo by Arturrro", link: "https://unsplash.com/photos/cooking-pot-with-powder-h2CPeqTzaaY"}
+                     ]
