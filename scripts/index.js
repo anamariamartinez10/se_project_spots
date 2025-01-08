dc599910bd7e7b4c427d406bfda5d89f02a5cd5f
AnaMM <amlcm31@gmail.com>
2025-01-08 06:53:09 -0700

    updated JS

--- scripts/index.js
+++ scripts/index.js
@@ -6,62 +6,92 @@ const initialCards = [{name: "Running photo by Martins Zemlickis", link: "https:
                       {name: "Bread photo by Arturrro", link: "https://unsplash.com/photos/cooking-pot-with-powder-h2CPeqTzaaY"}
                      ]
 
-
-//  Open Edit Modal on Click:
-
 const profileEditButton = document.querySelector(".profile__edit-button");
 const editProfileModal = document.querySelector("#edit-profile-modal");
+const editFormElement = editProfileModal.querySelector(".modal__form");
+const nameInput = editProfileModal.querySelector("#modal__input-name");
+const jobInput = editProfileModal.querySelector("#modal__input-description");
+const profileNameElement = document.querySelector(".profile__name");
+const profileJobElement = document.querySelector(".profile__subtitle");
+const closeProfileModal = document.querySelector(".modal__button-close");
+
+profileEditButton.addEventListener("click", openModal);
+closeProfileModal.addEventListener("click", closeModal);
+editFormElement.addEventListener("submit", handleEditFormSubmit);
 
 function openModal() {
     editProfileModal.classList.add("modal_opened");
+    nameInput.value = profileNameElement.textContent;
+    jobInput.value = profileJobElement.textContent;
 }
-
-profileEditButton.addEventListener("click", openModal);
-
-const closeProfileModal = document.querySelector(".modal__button-close");
 
 function closeModal () {
     editProfileModal.classList.remove("modal_opened");
 }
 
-closeProfileModal.addEventListener("click", closeModal);
-
-
-
-// Form Submission:
-
-// TODO: Find the form in the DOM.
-const profileFormElement = document.querySelector(".modal__form");
-
-// TODO: Find the form fields in the DOM.
-const nameInput = document.querySelector(".modal__input-name");
-const jobInput = document.querySelector(".modal__input-description");
-
-// TODO: Find the profile elements in the DOM
-const profileNameElement = document.querySelector(".profile__name");
-const profileJobElement = document.querySelector(".profile__subtitle");
-
-// The form submission handler. Note that its name 
-// starts with a verb and concisely describes what it does.
-function handleProfileFormSubmit(evt) {
-  // Prevent default browser behavior, see explanation below. 
+function handleEditFormSubmit(evt) {
   evt.preventDefault(); 
- 
-  // TODO: Get the values of each form field from the value property 
-  // of the corresponding input element.
-  // TODO: Then insert these new values into the textContent property of the 
-  // corresponding profile elements.
-  nameInput.value = profileNameElement.textContent;
-  jobInput.value = profileJobElement.textContent;
-  
-  // TODO: Close the modal.
+  profileNameElement.textContent = nameInput.value;
+  profileJobElement.textContent = jobInput.value;
   closeModal();
 }
 
-// Connect the handler to the form, so it will watch for the submit event.
-formElement.addEventListener('submit', handleProfileFormSubmit);
+
+// Render Cards:
+const cardTemplate = document.querySelector("#cardTemplate");
+const cardsList = document.querySelector(".cards__list");
+
+function getCardElement (data) {
+  const cardElement = cardTemplate.querySelector(".card").content.cloneNode(true);
+  const cardText = cardElement.querySelector(".card__text");
+  const cardImage = cardElement.querySelector(".card__image");
+  const cardAltText = cardImage.alt;
+  cardText.textContent = data.value;
+  cardImage.src = data.src;
+}
+
+for(let i = 0; i < initialCards.length; i++) {
+  const cardElement = getCardElement(initialCards[i]);
+  cardsList.prepend(cardElement);
+}
 
 
-// Render Cards
-// 1. Change HTML with <template>
-// 2. Write JS to change template
+// const submit = document.querySelector(".modal__button-save");
+// profileFormElement.addEventListener('submit', handleProfileFormSubmit);
+
+
+// Render Cards:
+// set the image’s src attribute to the image to the link field of the object
+// set the image’s alt text to the name field of the object
+// set the card’s title to the name field of the object, too
+// return the ready HTML element with the filled-in data
+// Iterate over the cards array using a loop, and in each iteration:
+
+// Pass the array item to your getCardElement() function to create a card element.
+// Use the appropriate built-in DOM method to add this HTML element to the page.
+
+
+// initialCards.forEach() {
+//   
+//   cardText.textContent = initialCards[i].name;
+//   cardImage.src = initialCards[i].link;
+// }
+
+
+
+
+
+// -------
+// const dataArray = [
+//   { title: 'Card 1', imageUrl: 'path/to/image1.jpg' },
+//   { title: 'Card 2', imageUrl: 'path/to/image2.jpg' },
+//   // more items...
+// ];
+
+// dataArray.forEach(item => {
+//   const template = document.querySelector('#card-template').content.cloneNode(true);
+//   template.querySelector('.card__title').textContent = item.title;
+//   template.querySelector('.card__image').src = item.imageUrl; // Assuming you have an <img> with class 'card__image'
+
+//   document.querySelector('.cards').appendChild(template);
+// });
